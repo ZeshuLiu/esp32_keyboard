f97be975446706c07574c965b642fce3ed55ef57
ZS Liu <100592697+LiuZSChina@users.noreply.github.com>
2022-11-19 14:48:29 +0800

    Delete Keyboard_code/poker_keyboard/build directory

--- Keyboard_code/poker_keyboard/build/libraries/BLE/BLEExceptions.cpp.d
+++
@@ -1,2 +0,0 @@
-d:\Data\¿ª·¢\esp32_keyboard\Keyboard_code\poker_keyboard\build\libraries\BLE\BLEExceptions.cpp.o: \
- C:\Users\LiuZeshu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\2.0.2\libraries\BLE\src\BLEExceptions.cpp
